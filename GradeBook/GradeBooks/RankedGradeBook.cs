5c4372933693427a561f073135b116ae139f8744
rwitek <rafal.witek@microsoft.wsei.edu.pl>
2023-03-14 20:39:55 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,5 +13,29 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+                throw new InvalidCastException();
+
+            if (averageGrade >= 80)
+            {
+                return 'A';
+            }
+            if (averageGrade < 80 && averageGrade >= 60)
+            {
+                return 'B';
+            }
+            if (averageGrade < 60 && averageGrade >= 40)
+            {
+                return 'C';
+            }
+            if (averageGrade < 40 && averageGrade >= 20)
+            {
+                return 'D';
+            }
+            else return 'F';
+        }
     }
 }
